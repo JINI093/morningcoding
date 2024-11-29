7749914c4dc9728a01038164e24b9e9b05b68bb2
JINI093 <koubook.jin@gamil.com>
2024-11-29 10:10:57 +0900

    pow함수로 변경

--- (50)Pow(x, n).dart
+++ (50)Pow(x, n).dart
@@ -1,19 +1,9 @@
-double myPow(double x, int n) {
-  // n이 0일 경우, 어떤 수의 0 제곱은 1
-  if (n == 0) return 1.0;
+import 'dart:math';
 
-  // n이 음수일 경우, x^n = 1 / (x^(-n))
-  if (n < 0) {
-    x = 1 / x;
-    n = -n;
+class Solution {
+  double myPow(double x, int n) {
+    // pow 함수 사용
+    num y = pow(x, n);
+    return y.toDouble();
   }
-
-  double result = 1.0;
-  double currentProduct = x;
-
-  for (int i = 0; i < n; i++) {
-    result *= currentProduct;
-  }
-
-  return result;
 }
